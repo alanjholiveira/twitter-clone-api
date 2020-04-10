21d757c1d925fe7143e387c1ec6239026f192940
Alan Oliveira <alanjhone@gmail.com>
2020-04-10 21:22:13 +0000

    Create routes applications

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,53 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
-Route.on('/').render('welcome');
+Route.post('/signup', 'UserController.signup').validator(['Signup']);
+Route.post('/login', 'UserController.login').validator(['Login']);
+
+// Account user
+Route.group(() => {
+  Route.get('/me', 'UserController.me');
+  Route.put('/update_profile', 'UserController.updateProfile').validator([
+    'UpdateProfile',
+  ]);
+  Route.put('/change_password', 'UserController.changePassword').validator([
+    'ChangePassword',
+  ]);
+})
+  .prefix('account')
+  .middleware(['auth']);
+
+// User use auth
+Route.group(() => {
+  Route.get('/timeline', 'UserController.timeline');
+  Route.get('/users_to_follow', 'UserController.usersToFollow');
+  Route.post('/follow', 'UserController.follow');
+  Route.delete('/unfollow/:id', 'UserController.unFollow');
+})
+  .prefix('users')
+  .middleware(['auth']);
+
+Route.post('/tweet', 'TweetController.store')
+  .middleware(['auth'])
+  .validator(['Tweet']);
+Route.get('/tweets/:id', 'TweetController.show');
+Route.post('/tweets/reply/:id', 'TweetController.reply')
+  .middleware(['auth'])
+  .validator(['TweetReply']);
+// Delete tweet
+Route.delete('/tweets/destroy/:id', 'TweetController.destroy').middleware([
+  'auth',
+]);
+
+// tweet reactions
+Route.group(() => {
+  // favorite tweet
+  Route.post('/create', 'FavoriteController.store');
+  // unfavorite tweet
+  Route.delete('/destroy/:id', 'FavoriteController.unFavorite');
+})
+  .prefix('favorites')
+  .middleware(['auth']);
+
+// User profile
+Route.get(':username', 'UserController.showProfile');
